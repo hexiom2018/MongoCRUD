27d65e3c88e9a4f06e814324448b823930d02dc8
Syed UZair Ali <maazuzair3322@gmail.com>
2019-05-21 20:26:47 +0500

    Home Screen ui complete

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 import Navigation from './navigation';
+import HomeBackground from './assets/homesrc.jpg'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Navigation/>
+      // <ImageBackground source={HomeBackground} style={{ width: '100%', height: '100%', flex: 1 }}>
+      //   <View style={styles.container}>
+      //  <Text style={styles.titleText}>
+      //     AppCars
+      //   </Text>
+      // </View>
+      
+       <Navigation/>
     );
   }
 }
@@ -13,8 +21,12 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'yellow',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,28 +1,129 @@
 import React from 'react';
-import { View, ScrollView, Image, Text, StyleSheet, TouchableOpacity, ActivityIndicator, RefreshControl, Button } from 'react-native';
-
+import { View, ScrollView, Image, Text, StyleSheet, TouchableOpacity, ImageBackground, StatusBar, Button } from 'react-native';
+import HomeBackground from '../../../assets/homesrc.jpg'
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        
+
         };
     }
-
+    static navigationOptions = {
+        // title: 'Welcome',
+        header: null
+    };
+    _onPressButton = ()=>{
+        console.log("function run")
+    }
     render() {
-      
+
         return (
-            <View> 
-                <Text>Hello world</Text>
+            <View style={styles.main}>
+                <StatusBar hidden={true} />
+                <ImageBackground source={HomeBackground} style={{ height: "100%", width: "100%" }}>
+                    <View style={styles.innerDiv} style={{ flex: 1, }}>
+                        <View style={styles.div1} >
+                            <Text style={styles.titleText}>
+                                AppCars
+                              </Text>
+                        </View>
+                        <View style={styles.div2}>
+                            <View style={styles.buttonRow1}>
+                                <TouchableOpacity style={styles.buttondiv1} onPress={this._onPressButton}>
+                                    <Text style={styles.buttonTittle}>
+                                        Creat
+                              </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.buttondiv2} onPress={this._onPressButton}>
+                                    <Text style={styles.buttonTittle}>
+                                        Read
+                                         </Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View style={styles.buttonRow2}>
+                                <TouchableOpacity style={styles.buttondiv3} onPress={this._onPressButton}>
+                                    <Text style={styles.buttonTittle}>
+                                        Update
+                              </Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.buttondiv4} onPress={this._onPressButton}>
+                                    <Text style={styles.buttonTittle}>
+                                        Delete
+                              </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+
+                </ImageBackground>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-   
+    main: {
+        backgroundColor: 'red',
+        flex: 1
+    },
+    titleText: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+    innerDiv: {
+        height: "100%", width: "100%",
+        flexDirection: 'column',
+    },
+    div1: {
+        height: '40%',
+        justifyContent: 'center',
+        alignItems: 'center',
 
+    }, div2: {
+        height: '60%',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+    },
+    buttonRow1: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        height: '23%',
+
+    }, buttonRow2: {
+        flexDirection: 'row',
+        justifyContent: "space-around",
+        height: '23%',
+    },
+    buttondiv1: {
+        backgroundColor: 'white',
+        width: '30%',
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttondiv2: {
+        backgroundColor: 'white',
+        width: '30%',
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, buttondiv3: {
+        backgroundColor: 'white',
+        width: '30%',
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, buttondiv4: {
+        backgroundColor: 'white',
+        width: '30%',
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }, buttonTittle: {
+        fontSize: 13,
+    }
 });
 
 export default Home;
