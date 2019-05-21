f87133555a10d7985139a9b6ec2b5ae3f3b9b5ef
Syed UZair Ali <maazuzair3322@gmail.com>
2019-05-21 17:42:08 +0500

    home screen work start

---
+++ src/screens/home/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, ScrollView, Image, Text, StyleSheet, TouchableOpacity, ActivityIndicator, RefreshControl, Button } from 'react-native';
+
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        
+        };
+    }
+
+    render() {
+      
+        return (
+            <View> 
+                <Text>Hello world</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+   
+
+});
+
+export default Home;
+
